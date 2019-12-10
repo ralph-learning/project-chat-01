972c3ecd706a1f2eceee1f9ae1e861b2d57723c4
Ralph Effting <ralfting@gmail.com>
2019-12-10 09:00:56 -0300

    Create a chat socket connection

--- lib/chat_project_web/channels/user_socket.ex
+++ lib/chat_project_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ChatProjectWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ChatProjectWeb.RoomChannel
+  channel "room:*", ChatProjectWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
