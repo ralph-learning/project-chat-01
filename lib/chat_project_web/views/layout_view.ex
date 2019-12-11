f4b8363d2f14eeb21ca7698973f1290b8341c2ae
Ralph Effting <ralfting@gmail.com>
2019-12-11 12:47:50 -0300

    Create helper logged_in in layout_view

--- lib/chat_project_web/views/layout_view.ex
+++ lib/chat_project_web/views/layout_view.ex
@@ -1,3 +1,15 @@
 defmodule ChatProjectWeb.LayoutView do
   use ChatProjectWeb, :view
+
+  def current_user(conn) do
+    conn
+    |> Plug.Conn.get_session(:user_id)
+    |> IO.inspect
+    |> case do
+      nil -> false
+      _id -> true
+    end
+  end
+
+  def logged_in?(conn), do: !!current_user(conn)
 end
